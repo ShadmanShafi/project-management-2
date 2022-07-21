57961868ebbcbb782bc7727fc3584613f56051b9
Shadman Shafi <shadmanshafi@iut-dhaka.edu>
2022-07-21 07:07:20 +0600

    HomePage Styling

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -2,8 +2,23 @@ import React from "react";
 
 export default function Home() {
   return (
-    <>
-      <div>Home</div>
-    </>
+    <div className="home">
+      <img
+        src={process.env.PUBLIC_URL + "logo.png"}
+        className="home-logo"
+        alt="logo"
+      />
+      <h2 className="home-title">Task management</h2>
+      <input
+        className="home-input"
+        type="text"
+        placeholder="Enter name"
+        name="name"
+        // value={form.name}
+        // onChange={onChangeInput}
+      />
+      <div class="home-error-alert">*Name cannot be empty</div>
+      <button class="home-btn">Submit</button>
+    </div>
   );
 }
