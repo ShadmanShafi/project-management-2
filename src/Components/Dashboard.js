1d7385da293681fb0590c91efaee60e046e1770c
Shadman Shafi <shadmanshafi@iut-dhaka.edu>
2022-07-22 08:00:44 +0600

    Implemented Dashboard Page

--- src/Components/Dashboard.js
+++ src/Components/Dashboard.js
@@ -1,15 +1,39 @@
 import React from "react";
-import { useUserContext } from "../ContextStore";
-import Header from "./Header";
-import Footer from "./Footer";
+import { useNavigate } from "react-router-dom";
 
 export default function Dashboard() {
-  const { name, setName } = useUserContext();
-  console.log(name);
+  const navigate = useNavigate();
+
+  const handleButtonClick = navigateTo => {
+    navigate(navigateTo);
+  }
 
   return (
     <div className="dashboard">
-      <div>Dashboard</div>
+      <div>
+        <p className="dashboard-bold-text">Welcome to task management system</p>
+        <br />
+        <p className="dashboard-text">
+          Lorem Ipsum is simply dummy text of the printing and typesetting
+          industry. Lorem Ipsum has been the industry's standard dummy text ever
+          since the 1500s, when an unknown printer took a galley of type and
+          scrambled it to make a type specimen book.
+        </p>
+        <br />
+        <br />
+        <br />
+        <br />
+        <p className="dashboard-bold-text">Get started</p>
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+      </div>
+      <div className="dashboard-buttons-flex">
+        <button className="dashboard-buttons" onClick={() => handleButtonClick('/tasks')}>Tasks</button>
+        <button className="dashboard-buttons" onClick={() => handleButtonClick('/members')}>Members</button>
+      </div>
     </div>
   );
 }
--- src/ContextStore.js
+++ src/ContextStore.js
@@ -1,4 +1,9 @@
-import { useState, useEffect, createContext, useContext } from "react";
+import {
+  useState,
+  useEffect,
+  createContext,
+  useContext,
+} from "react";
 import { useNavigate } from "react-router-dom";
 import { v4 } from "uuid";
 
@@ -17,13 +22,20 @@ export function MyProvider({ children }) {
   const [state, updateState] = useState(initialState);
   const navigate = useNavigate();
 
+  // const getLocalItems = () => {
+  //   let list = localStorage.getItem("taskManagementStore");
+  //   return list ? JSON.parse(list) : initialState;
+  // };
+
   useEffect(() => {
-    const adminStore = JSON.parse(localStorage.getItem("adminStore"));
-    if (adminStore) updateState(adminStore);
+    const taskManagementStore = JSON.parse(
+      localStorage.getItem("taskManagementStore")
+    );
+    if (taskManagementStore) updateState(taskManagementStore);
   }, []);
 
   useEffect(() => {
-    localStorage.setItem("adminStore", JSON.stringify(state));
+    localStorage.setItem("taskManagementStore", JSON.stringify(state));
   }, [state]);
 
   const setName = (newValue) => {
